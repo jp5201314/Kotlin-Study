2bdcff25077d5efdf0d70fbd13b810744a96b9f4
jiangping1314 <1160873948@qq.com>
2017-08-10 17:50:02 +0800

    增加数据类

---
+++ app/src/main/java/tinno/jiang/ping/kotlin/data/ForecastData.kt
@@ -0,0 +1,8 @@
+package tinno.jiang.ping.kotlin.data
+
+import java.util.*
+
+/**
+ * Created by android on 17-8-10.
+ */
+data class Forecast(val date: Date, val temperature: Float, val details: String)
